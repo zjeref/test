c4e5bea57907ba0e1783a584b10956f306bbf3ea
Ram <ramkr0072@gmail.com>
2023-11-10 18:12:35 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [reply, setReply] = useState("");
+
+  useEffect(() => {
+    const url = 'https://corsproxy.io/?' + encodeURIComponent('http://localhost:3000/api/v1/accounts/1/conversations/4/messages');
+    const headers = { api_access_token: "HeYAds24Px5WaGjDebPYjWtU" };
+    axios.get(url, { headers })
+      .then(res => {
+        console.log(res.data);
+        setMessages(res.data); // Assuming you want to update state with the fetched messages
+      })
+      .catch(error => {
+        console.error('Error making the request:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen w-screen flex justify-center items-center">
+      <div className='min-h-[600px] min-w-[400px] bg-black'>
+        <div className='w-full h-96 text-white'>
+          {/* Render messages here */}
+          {messages.map((message, index) => (
+            <p key={index}>{message}</p>
+          ))}
+        </div>
+        <div>
+          <input type="text" placeholder='enter chat' />
+          <button type='button' className='bg-white' onClick={() => { }}>Send</button>
+        </div>
+      </div>
     </div>
   );
 }
